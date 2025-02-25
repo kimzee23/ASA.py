478cf5d15bc2c3be1b7a48842a9306c876b77a5f
Gazar <98284057+kimzee23@users.noreply.github.com>
2025-02-25 02:37:39 +0100

    Add files via upload

---
+++ revaserTheStrings.py.py
@@ -0,0 +1,45 @@
+def run_in_palindrome(number)-> bool:
+    
+    number = int(input('Enter phone number:'))
+    reversing = number [ : : -1]
+    return True
+  
+    
+    return False
+    
+    
+
+name = "madam"
+     
+true = name [1: 5]
+false = name [5:0:-1]
+    
+print(true)
+print(false)
+
+ad = "ADIDAS"
+
+doing = ad [5:0:-1]
+
+print(doing)
+    
+  
+    
+   # if number == number[::-1]:
+     #   return True
+    #else:
+   #     return False
+        
+            
+            
+#number = int(input('Prompt user to enter a number:'))
+#num = run_in_palindrome(number)
+#print(num)
+
+
+
+
+    
+
+
+    
